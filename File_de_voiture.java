d3938e368fa3c49c104642d0a784484710940014
taumha <94001440+taumha@users.noreply.github.com>
2021-11-16 16:31:00 +0100

    Add files via upload

--- File_de_voiture.java
+++ File_de_voiture.java
@@ -1,6 +1,46 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class File_de_voiture {
-    int taille_file;
-    List Voiture;
+    private int Queue;
+    private List<Véhicule> véhiculeList=new ArrayList<Véhicule>();
+
+    public File_de_voiture(int q){
+        int Queue=q;
+    }
+
+    public void Enfiler(List<Véhicule> véhiculeList, Véhicule v){
+        véhiculeList.add(this.Queue, v);
+        this.Queue=this.Queue + 1;
+    }
+
+    public void Defiler(List<Véhicule> véhiculeList) throws Exception {
+        if (this.Queue==0){
+            throw new Exception ("La file est vide, impossible d'enlever des éléments");
+        }
+        else{
+            véhiculeList.remove(0);
+            this.Queue=this.Queue - 1;
+        }
+    }
+
+    public boolean EstVide(){
+        return (this.Queue==0);
+    }
+
+    public void setQueue(int queue) {
+        Queue = queue;
+    }
+
+    public void setVéhiculeList(List<Véhicule> véhiculeList) {
+        this.véhiculeList = véhiculeList;
+    }
+
+    public int getQueue() {
+        return Queue;
+    }
+
+    public List<Véhicule> getVéhiculeList() {
+        return véhiculeList;
+    }
 }
